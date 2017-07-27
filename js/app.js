caa706d801375f1456397bcbd61e5216a481c891
T-Wil <twil312@gmail.com>
2017-07-27 13:36:17 -0400

    updated handleItemDeletes function

--- js/app.js
+++ js/app.js
@@ -6,24 +6,29 @@ var state = {
 	itemsList: ["milk", "cheese"]
 };
 
+var listItemID = 'data-list-item-id';
+
  
 function handleItemAdds() {
 	$("#submitButton").click(function() {
-	event.preventDefault();
-	var newItem = $("#addItem").val();
-	addItems(state, newItem);
-	renderList(state, item);
-
+		event.preventDefault();
+		var newItem = $("#addItem").val();
+		console.log(newItem);
+		addItems(state, newItem);
+		renderList(state, item);
 	});
-}; 
+};
 
-/*function handleItemDeletes() {
-	deletes item from itemsList
-	document.getElementById("clearButton").addEventListener("click", function() {
-	 event.preventDefault();
-	 removeItems(state, indexNumber);
-});
-};*/
+function handleItemDeletes() {
+	//deletes item from itemsList
+	$(".deleteItem").click(function() {
+		event.preventDefault();
+		//use jquery to ID the data attribute for the item 
+		removeItems(state, indexNumber);
+		updateItem(state, itemIndex, newItemState);
+		//another event listener functon
+	};	
+};
 
 function addItems(state, item) {
 	state.itemsList.push(item);
@@ -33,14 +38,16 @@ function removeItems(state, indexNumber) {
 	state.itemsList.splice(indexNumber, 1);	
 };
 
-
+function updateItem(state, itemIndex, newItemState) {
+	state.items[itemIndex] = newItemState;
+}
 
 function renderList(state, item) {
 	var buildHtml = "<ul>";
 	  //Build HTML output for list// 
 	  for(var i=0;i < state.itemsList.length;i++) {
 	    
-	    buildHtml+="<div class='listCell'>" + "<li>" + "<button class='xButton'>" + "<i class='fa fa-check' aria-hidden='hidden'>" + "</button>" + "</i>" + state.itemsList[i] + "<button>" + "X" + 
+	    buildHtml+="<div class='listCell'>" + "<li>" + "<button class='xButton'>" + "<i class='fa fa-check' aria-hidden='hidden'>" + "</button>" + "</i>" + state.itemsList[i] + "<button class='deleteItem'>" + "X" + 
 	    "</button>" + "</li>" + "</div";
 	    
 	  } 
